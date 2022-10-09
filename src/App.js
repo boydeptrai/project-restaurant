90baf0e6c7cb2d8914458b17bd518596ea698964
Tuan Tran <langthamht047@gmail.com>
2022-10-09 07:31:47 -0700

    building header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Header from './container/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
