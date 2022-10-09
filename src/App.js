5dbe979963bd24b3de64759c26a98d3505ab74e0
Tuan Tran <langthamht047@gmail.com>
2022-10-09 08:30:05 -0700

    building aboutus

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './container/Header/Header';
+import AboutUs from './container/AboutUs/AboutUs'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <AboutUs />
     </div>
   );
 }
